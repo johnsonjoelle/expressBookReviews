50335996d97de5c89f87000c8673f4df07d7cc3e
Joelle Johnson <contact@joellejohnson.com>
2025-06-27 19:16:09 -0400

    Added review change functionality

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -18,19 +18,54 @@ const isValid = (username)=>{ //returns boolean
 }
 
 const authenticatedUser = (username,password)=>{ //returns boolean
-//write code to check if username and password match the one we have in records.
+  let validUsers = users.filter(user => {
+    return (user.username === username && user.password === password);
+  });
+  if (validUsers.length > 0) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 //only registered users can login
 regd_users.post("/login", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const username = req.body.username;
+  const password = req.body.password;
+  // Check if username and password were entered
+  if ( !username || !password ) {
+    return res.status(404).json({message: "Username and password required"})
+  }
+  // Authenticate user
+  if( authenticatedUser(username, password) ) {
+    let accessToken = jwt.sign({
+        data: password
+    }, 'access', { expiresIn: 60 * 60 });
+    req.session.authorization = {accessToken, username};
+    return res.status(200).json({message: "User successfully logged in"});
+  } else {
+    return res.status(208).json({message: "Invalid login: Check username and password"});
+  }
 });
 
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  // Check if user has left a review on the book
+  const userAuth = req.user; // from auth function in index.js
+  const isbn = req.params.isbn;
+  const review = req.query.review;
+  const user = req.session.authorization.username;
+  let reviews = books[isbn];
+  let userReview = reviews.user;
+  if (!userAuth) {
+    return res.status(404).json({message: "User must be logged in"});
+  }
+  if (user && isbn && review) {
+    books[isbn]["reviews"][user] = review;
+    res.send(books[isbn]);
+  } else {
+    return res.status(404).json({message: "Error uploading review"});
+  }
 });
 
 module.exports.authenticated = regd_users;
